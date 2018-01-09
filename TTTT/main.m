2b7b63cf7a405af3e61dfec2fa9ccc71e7beafac
lichaoqun <121708983@qq.com>
2018-01-09 21:27:19 +0800

    Initial Commit

---
+++ TTTT/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TTTT
+//
+//  Created by QG on 2018/1/9.
+//  Copyright © 2018年 xinxianzhizao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
